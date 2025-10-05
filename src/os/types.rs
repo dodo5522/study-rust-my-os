8170855cc25320818b8350631587271d0d6845e3
Takashi Ando <dodo5522@gmail.com>
2025-10-05 22:14:00 +0900

    [env] formatter

--- src/os/types.rs
+++ src/os/types.rs
@@ -8,32 +8,32 @@ pub type Result<T> = core::result::Result<T, &'static str>;
 #[must_use]
 #[repr(u64)]
 pub enum EfiStatus {
-    Success = 0,
+  Success = 0,
 }
 
 #[repr(C)]
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub struct EfiGuid {
-    pub data0: u32,
-    pub data1: u16,
-    pub data2: u16,
-    pub data3: [u8; 8],
+  pub data0: u32,
+  pub data1: u16,
+  pub data2: u16,
+  pub data3: [u8; 8],
 }
 
 #[repr(C)]
 pub struct EfiBootServicesTable {
-    _reserved0: [u64; 40],
-    pub locate_protocol: extern "win64" fn(
-        protocol: *const EfiGuid,
-        registration: *const EfiVoid,
-        interface: *mut *mut EfiVoid,
-    ) -> EfiStatus,
+  _reserved0: [u64; 40],
+  pub locate_protocol: extern "win64" fn(
+    protocol: *const EfiGuid,
+    registration: *const EfiVoid,
+    interface: *mut *mut EfiVoid,
+  ) -> EfiStatus,
 }
 const _: () = assert!(offset_of!(EfiBootServicesTable, locate_protocol) == 320);
 
 #[repr(C)]
 pub struct EfiSystemTable {
-    _reserved0: [u64; 12],
-    pub boot_services: &'static mut EfiBootServicesTable,
+  _reserved0: [u64; 12],
+  pub boot_services: &'static mut EfiBootServicesTable,
 }
 const _: () = assert!(offset_of!(EfiSystemTable, boot_services) == 96);
